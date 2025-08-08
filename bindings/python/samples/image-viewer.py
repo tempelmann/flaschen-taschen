4a5b8d81d9a98c422b8585a6289f2ec9864585c3
Thomas Tempelmann <tempelmann@gmail.com>
2025-08-08 16:55:05 +0200

    some more python examples added by me

---
+++ bindings/python/samples/image-viewer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import time
+import sys
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
+
+if len(sys.argv) < 2:
+    sys.exit("Require an image argument")
+else:
+    image_file = sys.argv[1]
+
+image = Image.open(image_file)
+
+# Configuration for the matrix
+import matrixOptions
+options = matrixOptions.matrixOptions()
+
+matrix = RGBMatrix(options = options)
+
+# Make image fit our screen.
+image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+matrix.SetImage(image.convert('RGB'))
+
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
