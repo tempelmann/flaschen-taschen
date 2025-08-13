2cf6705b7504f8b1c872613263d3179afb02090e
Thomas Tempelmann <tempelmann@gmail.com>
2025-08-13 04:06:06 +0200

    Changes the demo code to using a 256x256 image.

--- examples-api-use/simple-example.py
+++ examples-api-use/simple-example.py
@@ -4,11 +4,11 @@ import flaschen
 UDP_IP = 'localhost'
 UDP_PORT = 1337
 
-ft = flaschen.Flaschen(UDP_IP, UDP_PORT, 45, 35)
+ft = flaschen.Flaschen(UDP_IP, UDP_PORT, 256, 256)
 
 while True:
   for b in range(0, 256):
     for y in range(0, ft.height):
       for x in range(0, ft.width):
-        ft.set(x, y, ((x * 255) // 45, (y * 255) // 35, b))
+        ft.set(x, y, ((x * 255) // 256, (y * 255) // 256, b))
     ft.send()
