942d493d0a4f150fa57f745ddffe47d16418f8fe
Thomas Tempelmann <tempelmann@gmail.com>
2026-05-09 17:28:56 +0200

    Improves example(s)

---
+++ client/demo.sh
@@ -0,0 +1,1 @@
+./send-text -h pi4 -f fonts/10x20.bdf "test"
--- examples-api-use/simple-example.py
+++ examples-api-use/simple-example.py
@@ -1,14 +1,20 @@
 # (Hey Scotty, how can I set the 'include' path for python ?)
 import flaschen
+import time
 
-UDP_IP = 'localhost'
+# Getting "Message too long"? Need to fix the max UDP size – see client/README.md
+
+UDP_IP = 'pi4'
 UDP_PORT = 1337
 
-ft = flaschen.Flaschen(UDP_IP, UDP_PORT, 256, 256)
+N = 256
+
+ft = flaschen.Flaschen(UDP_IP, UDP_PORT, N, N)
 
 while True:
   for b in range(0, 256):
     for y in range(0, ft.height):
       for x in range(0, ft.width):
-        ft.set(x, y, ((x * 255) // 256, (y * 255) // 256, b))
+        ft.set(x, y, ((x * 255) // N, (y * 255) // N, b))
     ft.send()
+    #time.sleep(0.003)
